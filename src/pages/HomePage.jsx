273fae0d7a6c45f7d1bd481feebe168c5e60017e
andreas893 <eaa24andrh@students.eaaa.dk>
2025-10-03 10:09:50 +0200

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,8 @@
 export default function HomePage() {
-    return (
-        <section className="page">
-            <h1>Home Page</h1>
-            <p>Home is where the heart is 💛</p>
-            <p>Oh My, sounds like a bad movie!</p>
-        </section>
-    );
+  return (
+    <section className="page">
+      <h1>Min Første React App på GitHub Pages! 🚀</h1>
+      <p>Dette er en moderne React SPA bygget med Vite og React Router.</p>
+    </section>
+  );
 }
