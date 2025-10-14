d25b4a7062fae99453a0efe678e516e920756ef2
mathiaslyhr <mathiaslyhroutlook.com>
2025-10-14 11:19:40 +0200

    Fjernet ubrugte referencer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { Routes, Route, Navigate } from "react-router";
 import Nav from "./components/Nav";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
 import Footer from "./components/Footer";
-import ServicesPage from "./pages/ServicesPage";
 
 export default function App() {
   return (
