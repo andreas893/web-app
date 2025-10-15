1b225642d6116d721de7faaf8b90ec5d31f9bf16
andreas893 <eaa24andrh@students.eaaa.dk>
2025-10-15 12:58:49 +0200

    tilføjet onboarding page og påbegyndt undersider til

---
+++ src/components/StepAge.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StepAge = () => {
+  return (
+    <div>StepAge</div>
+  )
+}
+
+export default StepAge
---
+++ src/components/StepDiscover.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StepDiscover = () => {
+  return (
+    <div>StepDiscover</div>
+  )
+}
+
+export default StepDiscover
