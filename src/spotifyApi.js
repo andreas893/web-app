e172b4856557c97e76a7f3798435e7f3b5836124
andreas893 <eaa24andrh@students.eaaa.dk>
2025-10-28 14:11:09 +0100

    tilføjet spotify api recommended

--- src/spotifyApi.js
+++ src/spotifyApi.js
@@ -18,4 +18,28 @@ export async function searchSpotifyTracks(query, token) {
     const data = await res.json();
     return data.tracks.items;
   }
+
+  export async function fetchSpotifyRecommendations(token, moodGenre) {
+  const res = await fetch(
+    `https://api.spotify.com/v1/recommendations?limit=5&seed_genres=${moodGenre}`,
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+
+  if (!res.ok) {
+    console.error("Spotify recommendations error:", res.statusText);
+    return [];
+  }
+
+  const data = await res.json();
+
+  return data.tracks.map(track => ({
+    id: track.id,
+    title: track.name,
+    artist: track.artists[0]?.name,
+    coverUrl: track.album.images[0]?.url,
+    previewUrl: track.preview_url,
+  }));
+}
   
