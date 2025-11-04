39a413dec192d7d6dde1568edb1606a7ec4ca8c5
andreas893 <eaa24andrh@students.eaaa.dk>
2025-11-04 12:44:24 +0100

    cta forside

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,19 +1,31 @@
+import { useState } from "react";
 import SoundForYou from "../components/SoundForYou";
 import Feed from "../components/Feed";
 import Wrapped from "../components/Wrapped";
 import FooterNav from "../components/FooterNav";
+import CreatePlaylistPopup from "../components/CreatePlaylistPopup";
 
 export default function LandingPage() {
+  const [popupType, setPopupType] = useState(null);
   return (
     <div className="bg-[#121212] min-h-screen flex flex-col font-inter relative">
       <main className="flex-1 pb-20 overflow-y-auto hide-scroll px-4">
         <div className="logo-header">
           <img src="/images/chord-logo.png" alt="Chord logo" />
+           <button onClick={() => setPopupType("create")} className="create-playlist-btn">
+           <span>＋</span>
+             </button>
         </div>
         <Feed />
         <SoundForYou />
         <Wrapped />
       </main>
+       {popupType === "create" && (
+        <CreatePlaylistPopup
+          type="create"
+          onClose={() => setPopupType(null)}
+        />
+      )}
       <FooterNav />
     </div>
   );
